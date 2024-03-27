5d7bcf6a5f43601a49eef48726393290fef3f389
sergbash1991 <sergbash1991@gmail.com>
2024-03-27 19:31:58 +0200

    Fixed Home Work 2024.03.25

--- task_Solar_2024_03_25.py
+++ task_Solar_2024_03_25.py
@@ -7,14 +7,18 @@ class Planet:
 
     def __setattr__(self, key, value):
         dict_types = {"_name": str, "_diametr": int | float, "_period_solar": float, "_period": int | float}
-        if dict_types[key] == value:
+        if isinstance(value, dict_types[key]):
             if key in ("_diametr", "_period_solar", "_period") and value <= 0:
                 raise ValueError("Diameter, period solar and period must be positive")
             object.__setattr__(self, key, value)
 
+    def __str__(self):
+        return f"Planet {self._name}, diametr {self._diametr}, period solar {self._period_solar}, period {self._period}"
+
 
 class SolarSystem:
     __instance = None
+    __counter = 0
 
     __slots__ = ('_mercury', '_venus', '_earth', '_mars', '_jupiter', '_saturn', '_uranus', '_neptune')
 
@@ -28,10 +32,23 @@ class SolarSystem:
             cls.__instance._jupiter = Planet("Юпитер", 142984, 4332.59, 0.412)
             cls.__instance._saturn = Planet("Сатурн", 120536, 10759.22, 0.428)
             cls.__instance._uranus = Planet("Уран", 51118, 30688.5, 0.718)
-            cls.__instance._neptune = Planet("Нептун", 49528, 60182, 0.671)
+            cls.__instance._neptune = Planet("Нептун", 49528, 60.182, 0.671)
         return cls.__instance
+
+    def __iter__(self):
+        SolarSystem.__counter = 0
+        return self
+
+    @classmethod
+    def __next__(cls):
+        components = cls.__slots__
+        if len(components) == cls.__counter:
+            raise StopIteration
+        component = getattr(cls.__instance, components[cls.__counter])
+        cls.__counter += 1
+        return component
 
 
 solar_system = SolarSystem()
-print(solar_system)
-
+for planet in solar_system:
+    print(planet)
